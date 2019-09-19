19fc060e2c6065fa6a17db0b22b0ee9fa5da7d75
hisnameispum <sirasitxp@gmail.com>
2019-09-19 01:37:07 -0700

    Add Break, Continue Example

--- midterm1.py
+++ midterm1.py
@@ -7,3 +7,17 @@ print("Widget")
 list = ["Everything","is","a","Widget"]
 for counter, value in enumerate(list):
     print(f"{counter}: {value}")
+
+# Break Statement
+for i in range(1, 11):
+    if i is 5:
+        break
+    else:
+        print(i)
+
+# Continue Statement
+for i in range(1,11):
+    if i is 5:
+        continue
+    else:
+        print(i)
